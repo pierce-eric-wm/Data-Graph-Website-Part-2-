69cb8e1513f25e0c44889c5645890d5bf15390e7
Eric <eric-pierce@west-mec.org>
2015-10-22 15:11:59 -0700

    Sorry, i did all of the project on a separate folder so you won't see any commitments but i had a few in my other project file.

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by session1 on 10/22/15.
+ */
+// not a finished product
+
+
+$(document).ready(function() {
+    var name= prompt ("What is your name?");
+    alert( "Hello!" + name);
+    $('#userName').html( name );
+});
